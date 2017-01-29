da4ba8bfd6957455beb5c596b26973f3253584e8
sequel7 <ronin7@calima7.net>
2017-01-29 14:55:40 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,4 +5,4 @@ cat /opt/factorio/data/server-settings.example.json | sed s/"GAMENAME"/"$FACTORI
 if [ ! -f $SAVEFILE ]; then
   /opt/factorio/bin/x64/factorio --create $SAVEFILE --server-settings /opt/factorio/data/server-settings.json
 fi
-/opt/factorio/bin/x64/factorio --start-server $SAVEFILE --server-settings /opt/factorio/data/server-settings.json
+/opt/factorio/bin/x64/factorio --start-server $SAVEFILE --server-settings /opt/factorio/data/server-settings.json --port $FACTORIO_SERVER_PORT
