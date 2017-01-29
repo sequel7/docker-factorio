ba898b948a7ed61994a80f84ea85bcd58c32e0af
sequel7 <ronin7@calima7.net>
2017-01-29 06:06:20 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [ ! -f $SAVEFILE ]; then
-  /opt/factorio/bin/x64/factorio --create $SAVEFILE -c /opt/factorio/server-settings.json
+  /opt/factorio/bin/x64/factorio --create $SAVEFILE -c /opt/factorio/data/server-settings.json
 fi
-/opt/factorio/bin/x64/factorio --start-server $SAVEFILE -c /opt/factorio/server-settings.json
+/opt/factorio/bin/x64/factorio --start-server $SAVEFILE -c /opt/factorio/data/server-settings.json
